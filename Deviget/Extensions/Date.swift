5605def3496946fccf22be9feb7276f4837b67ed
Fernando Gallo <fernandgallo@gmail.com>
2019-10-21 14:25:18 -0400

    Date extension to show time ago string

---
+++ Deviget/Extensions/Date.swift
@@ -0,0 +1,38 @@
+//
+//  Date.swift
+//  Deviget
+//
+//  Created by Fernando Gallo on 21/10/19.
+//  Copyright © 2019 Fernando Gallo. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+    func timeAgo() -> String {
+        let interval = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: self, to: Date())
+        if let year = interval.year, year > 0 {
+            return year == 1 ? "\(year)" + " " + "year" :
+                "\(year)" + " " + "years"
+        } else if let month = interval.month, month > 0 {
+            return month == 1 ? "\(month)" + " " + "month" :
+                "\(month)" + " " + "months"
+        } else if let day = interval.day, day > 0 {
+            return day == 1 ? "\(day)" + " " + "day" :
+                "\(day)" + " " + "days"
+        } else if let hour = interval.hour, hour > 0 {
+            return hour == 1 ? "\(hour)" + " " + "hour" :
+                "\(hour)" + " " + "hours"
+        } else if let minute = interval.minute, minute > 0 {
+            return minute == 1 ? "\(minute)" + " " + "minute" :
+                "\(minute)" + " " + "minutes"
+        } else if let second = interval.second, second > 0 {
+            return second == 1 ? "\(second)" + " " + "second" :
+                "\(second)" + " " + "seconds"
+        } else {
+            return "a moment ago"
+        }
+    }
+    
+}
