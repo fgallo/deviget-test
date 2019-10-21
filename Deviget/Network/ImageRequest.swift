33ff8eada9792aca5e0887d62011bcc71f3652e4
Fernando Gallo <fernandgallo@gmail.com>
2019-10-21 14:52:55 -0400

    ImageRequest class to download thumbnail images

---
+++ Deviget/Network/ImageRequest.swift
@@ -0,0 +1,28 @@
+//
+//  ImageRequest.swift
+//  Deviget
+//
+//  Created by Fernando Gallo on 21/10/19.
+//  Copyright © 2019 Fernando Gallo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageRequest {
+    let url: URL
+    
+    init(url: URL) {
+        self.url = url
+    }
+}
+
+extension ImageRequest: NetworkRequest {
+    func decode(_ data: Data) -> UIImage? {
+        return UIImage(data: data)
+    }
+    
+    func load(withCompletion completion: @escaping (UIImage?) -> Void) {
+        load(url, withCompletion: completion)
+    }
+}
